c8a8ce07591cb9e12de4577127081a1b3ba86981
vlasevich <vlasentiy@gmail.com>
2016-08-23 22:14:17 +0300

    added Google Map, added some markers to the map;

--- app/src/main/java/com/home/vlas/shops/fragment/MapFragment.java
+++ app/src/main/java/com/home/vlas/shops/fragment/MapFragment.java
@@ -3,11 +3,21 @@ package com.home.vlas.shops.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.home.vlas.shops.R;
 import com.home.vlas.shops.db.DataBaseHelper;
 import com.home.vlas.shops.model.Instrument;
@@ -16,6 +26,7 @@ public class MapFragment extends AbstractTabFragment {
     private static final int LAYOUT = R.layout.fragment_map;
     private static final String TAG = MapFragment.class.getSimpleName();
     private DataBaseHelper db;
+    private SupportMapFragment mSupportMapFragment;
 
     public static MapFragment getInstance(Context context) {
         Bundle args = new Bundle();
@@ -34,9 +45,43 @@ public class MapFragment extends AbstractTabFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
-        if (getUserVisibleHint()) {
+        if (true) {
             Log.i(TAG, "RUN");
             runBD();
+
+            mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapwhere);
+            if (mSupportMapFragment == null) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                mSupportMapFragment = SupportMapFragment.newInstance();
+                fragmentTransaction.replace(R.id.mapwhere, mSupportMapFragment).commit();
+            }
+
+            if (mSupportMapFragment != null) {
+                mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        if (googleMap != null) {
+
+                            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                            googleMap.getUiSettings().setAllGesturesEnabled(true);
+
+                            //-> marker_latlng // MAKE THIS WHATEVER YOU WANT
+
+                            CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(17.385044, 78.486671)).zoom(15.0f).build();
+                            CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+
+                            googleMap.addMarker(new MarkerOptions().position(new LatLng(48.441928, 35.021000)).title("HOME")).showInfoWindow();
+                            googleMap.addMarker(new MarkerOptions().position(new LatLng(48.448309, 35.025123)).title("NEW HOME")).showInfoWindow();
+
+                            googleMap.moveCamera(cameraUpdate);
+                            //48.441928, 35.021000
+
+                        }
+
+                    }
+                });
+            }
         } else {
             Log.i(TAG, "NOT RUN");
         }
